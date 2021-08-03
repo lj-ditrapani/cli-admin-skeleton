0ae510fb979003dd4f5ec17e28dffeb3e2b5e893
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2021-08-03 01:27:24 -0400

    Exercise the libraries

--- src/main/scala/info/ditrapani/Main.scala
+++ src/main/scala/info/ditrapani/Main.scala
@@ -1,5 +1,30 @@
 package info.ditrapani
 
+import com.typesafe.config.ConfigFactory
+import sttp.client3._
+import sttp.client3.httpclient.HttpClientFutureBackend
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+
 object Main extends App {
+  val config = ConfigFactory.load()
   println("Hello")
+  println(config.getString("foo"))
+
+  val sort: Option[String] = None
+  val query = "http language:scala"
+
+  val request =
+    basicRequest.get(uri"https://api.github.com/search/repositories?q=$query&sort=$sort")
+
+  val backend = HttpClientFutureBackend()
+  val future = request.send(backend)
+
+  future.foreach { response =>
+    {
+      println(response.headers)
+    }
+  }
+  Await.ready(future, 100.seconds)
 }
